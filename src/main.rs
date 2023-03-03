c351cd0ac7d6aa595f21d1aaafea7761bf7872dd
robinbreast <kihoa.nam@gmail.com>
2023-03-04 00:25:07 +0100

    define command line arguments and json file format

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,58 @@ use anyhow::{Context, Result};
 use clap::Parser;
 use std::fs;
 use std::path;
+use serde::Deserialize;
 
+/// tmpgen command line arguments
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
-
 struct Args {
-    /// input data file path
+    /// input content json file name
     #[clap(short, long)]
-    input: String,
+    content_filename: String,
     /// template directory
-    temp_dir: String,
+    #[clap(short, long)]
+    template_dirname: String,
     /// output directory
-    output_dir: String,
+    #[clap(short, long)]
+    output_dirname: String,
+}
+
+/// tmpgen content json file format
+#[derive(Debug, Deserialize)]
+struct Data {
+    name: String,
+    value: String,
+}
+#[derive(Debug, Deserialize)]
+struct Group {
+    name: String,
+    data: Vec<Data>,
+}
+#[derive(Debug, Deserialize)]
+struct Content {
+    #[serde(default="default_dataset")]
+    dataset: Vec<Data>,
+    #[serde(default="default_groupset")]
+    groupset: Vec<Group>,
+}
+fn default_dataset() -> Vec<Data> {
+    Vec::new()
+}
+fn default_groupset() -> Vec<Group> {
+    Vec::new()
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
-    if path::Path::new(&args.output_dir).exists() {
+    if path::Path::new(&args.output_dirname).exists() {
         println!(
             "`{}` is already existed. select other folder as output",
-            args.output_dir
+            args.output_dirname
         );
     } else {
-        fs::create_dir_all(path)
+        fs::create_dir_all(&args.output_dirname)
+            .with_context(|| format!("failed to create folder `{}`", args.output_dirname))?;
     }
     Ok(())
 }
